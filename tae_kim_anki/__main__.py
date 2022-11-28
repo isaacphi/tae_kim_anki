f0921a7816c7bbae1c17b98be9c42d7b3e3c62d8
Phil <phil@pheedloop.com>
2022-11-27 19:41:37 -0500

    parse single page

--- tae_kim_anki/__main__.py
+++ tae_kim_anki/__main__.py
@@ -1,9 +1,23 @@
 import csv
 import requests
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
 
 
 OUTPUT_FILENAME = "out.csv"
+START_URL = "https://guidetojapanese.org/learn/grammar/stateofbeing"
+
+
+@dataclass
+class Example:
+    """Corresponds to one anki card"""
+
+    english: str
+    japanese: str
+    vocab: list[dict[str, str]]
+    section: str = ""
+    chapter: str = ""
+    link: str = ""
 
 
 def write_csv_file(rows: list[list[str]]) -> None:
@@ -15,15 +29,58 @@ def write_csv_file(rows: list[list[str]]) -> None:
             writer.writerow(row)
 
 
-def parse_webpage():
-    web_url = "https://www.geeksforgeeks.org/"
+def create_example_from_section(example_section):
+    """
+    Example li structure
+    <li><span class="popup" title="ともだち - friend">友達</span><em>じゃない</em>。<br/>
+    """
+    flattened_string = "".join([s for s in example_section.strings])
+    [jp, en] = flattened_string.split("\n")
+    vocab = []
+    for vocab_element in example_section.find_all("span"):
+        kanji = vocab_element.string
+        explanation = vocab_element.get("title")
+        vocab.append({"kanji": kanji, "explanation": explanation})
+    section = example_section.find_parent("h2")
+
+    return Example(english=en, japanese=jp, vocab=vocab)
+
+
+def parse_webpage(web_url):
     html = requests.get(web_url).content
-    soup = BeautifulSoup(html, "html.parser")
-    print(soup)
+    page = BeautifulSoup(html, "html.parser")
+
+    examples = []
+    chapter = page.find("h1").string
+
+    # Find all headings for "Examples" sections
+    sub_titles = page.find_all("h3")
+    for sub_title in sub_titles:
+        if sub_title.contents and sub_title.contents[0] == "Examples":
+            section_name = sub_title.find_previous_sibling("h2").string
+            # Create Example objects from each list item
+            example_list = sub_title.find_next_sibling("ol")
+            for example_section in example_list.find_all("li"):
+                example = create_example_from_section(example_section)
+                example.section = section_name
+                example.chapter = chapter
+                example.link = web_url
+                examples.append(example)
+
+    next_url = ""
+    next_url_element = page.find("span", class_="series-nav-right").find("a")
+    if next_url_element:
+        next_url = next_url_element.get("href")
+
+    return examples, next_url
 
 
 def main():
-    parse_webpage()
+    examples, next_url = parse_webpage(START_URL)
+    for e in examples:
+        print(e)
+
+    print(next_url)
 
     # write_csv_file([["a", "b", "c"], ["1", "2", "3"]])
 
